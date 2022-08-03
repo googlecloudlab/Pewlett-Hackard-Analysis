2fe6218d92be41a3306cbe66c98d80ddb3f0bbfe
Robert Loesch <rloesch@google.com>
2022-08-03 11:47:46 +0200

    Mentorship Eligibility

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -31,3 +31,22 @@ INTO retiring_titles
 FROM unique_titles
 GROUP BY title
 ORDER BY count DESC
+
+-- create a Mentorship Eligibility table for current employees 
+-- who were born between January 1, 1965 and December 31, 1965
+SELECT DISTINCT ON (e.emp_no) e.emp_no, 
+                   e.first_name, 
+				   e.last_name, 
+				   e. birth_date,
+				   de.from_date,
+				   de.to_date,
+				   t.title
+INTO mentorship_eligibility
+FROM employees as e
+LEFT JOIN dept_employee AS de
+ON e.emp_no = de.emp_no
+LEFT JOIN titles as t
+ON e.emp_no = t.emp_no
+WHERE (de.to_date = '9999-01-01')
+AND (birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no ASC; 
